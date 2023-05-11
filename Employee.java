82fdb518a758332be1457210422fed0ab0cd9b68
Aravind chamaku <iamarvind006@gmail.com>
2023-05-11 08:42:57 +0530

    Added company

--- Employee.java
+++ Employee.java
@@ -2,6 +2,7 @@ public class Employee {
     static String name = "John Smith";
     static int age = 48;
     static String city = "Hyd";
+    static String company = "JPMC";
 
     public static void main(String[] args) {
         System.out.println("Name: " + name + " age: " + age + " city: " + city);
